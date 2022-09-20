3b3c05834f0bf8d62b203860ff1838a7989fca37
Ik <113038466+Ik1497@users.noreply.github.com>
2022-09-20 22:17:33 +0200

    Update Website

---
+++ assets/footer.js
@@ -0,0 +1,12 @@
+document.write('\
+\
+<footer data-booted="true">\
+    <div>\
+        <span>© 2022 Streamer.bot Actions. All rights reserved. |&nbsp;</span>\
+        <span>Made by\
+            <a href="https://github.com/ik1497">Ik1497</a>\
+        </span>\
+    </div>\
+</footer>\
+\
+');
---
+++ assets/head.js
@@ -0,0 +1,11 @@
+document.write('\
+\
+<meta name="og:site_name" content="Streamer.bot Actions"/>\
+<meta property="og:type" content="website" />\
+<meta property="og:image" content="https://raw.githubusercontent.com/Ik1497/Ik1497/main/assets/profile_picture/Logo-Round.png" />\
+<link rel="icon" href="https://raw.githubusercontent.com/Ik1497/Ik1497/main/assets/profile_picture/Logo-Round.png" />\
+<meta name="theme-color" content="#B80086">\
+<link rel="stylesheet" href="/assets/style.css">\
+<script src="/assets/navigation.js"></script>\
+\
+');
