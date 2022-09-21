b733490133b74cf76f8b25b244e51eea667665a1
Ik <113038466+Ik1497@users.noreply.github.com>
2022-09-21 23:20:24 +0200

    Started Added a first Extension

--- assets/navigation.js
+++ assets/navigation.js
@@ -2,11 +2,8 @@ document.write('\
 \
 <ul class="navbar">\
 <a href="/index.html"><li>Home</li></a>\
-<details><summary>Utility Extensions</summary>\
-  <a href="/Utility/Formatting_Page.html"><li>Formatting Page</li></a>\
-</details>\
-<details><summary>Socials</summary>\
-  <a href="/Socials/Discord.html" target="_blank"><li>Discord</li></a>\
+<details><summary>Extensions</summary>\
+  <a href="/Extensions/Discord-Webhooks.html"><li>Discord Webhooks</li></a>\
 </details>\
 <a href="/About.html"><li>About</li></a>\
 </ul>\
