3cd017789b9f39650ed53479baec15a77b936125
Ik <113038466+Ik1497@users.noreply.github.com>
2022-09-21 21:08:06 +0200

    Loads Of CSS Changes Including MDI Icons

--- assets/footer.js
+++ assets/footer.js
@@ -1,11 +1,15 @@
 document.write('\
 \
-<footer data-booted="true">\
+<footer>\
     <div>\
         <span>© 2022 Streamer.bot Actions. All rights reserved. |&nbsp;</span>\
         <span>Made by\
             <a href="https://github.com/ik1497">Ik1497</a>\
         </span>\
+    </div>\
+</footer>\
+<footer>\
+    <div>\
     </div>\
 </footer>\
 \
