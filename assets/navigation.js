d29b7c93d7ac6304a9131cee7dd075983f9c2a86
Ik <113038466+Ik1497@users.noreply.github.com>
2022-09-21 12:53:14 +0200

    Update navigation.js

--- assets/navigation.js
+++ assets/navigation.js
@@ -7,7 +7,6 @@ document.write('\
 </details>\
 <details><summary>Discord</summary>\
   <a href="https://ik1497.github.io/Discord"><li>Invite Link</li></a>\
-  <a><li>Server Rules</li></a>\
 </details>\
 <a href="/About.html"><li>About</li></a>\
 </ul>\
