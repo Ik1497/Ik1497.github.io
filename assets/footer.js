efd54b4e775eea9d0f2c511a43ac3be87eec6115
Ik <113038466+Ik1497@users.noreply.github.com>
2022-09-21 22:03:58 +0200

    Added Most Links Of The Footer

--- assets/footer.js
+++ assets/footer.js
@@ -1,16 +1,23 @@
 document.write('\
 \
 <footer>\
-    <div>\
-        <span>© 2022 Streamer.bot Actions. All rights reserved. |&nbsp;</span>\
-        <span>Made by\
-            <a href="https://github.com/ik1497">Ik1497</a>\
-        </span>\
-    </div>\
+\
+  <div class="upper">\
+      <span>© 2022 Streamer.bot Actions. All rights reserved. |&nbsp;</span>\
+      <span>Made by\
+          <a href="https://github.com/ik1497">Ik1497</a>\
+      </span>\
+  </div>\
 </footer>\
 <footer>\
-    <div>\
-    </div>\
+  <div class="lower">\
+      <nav>\
+        <a href="/Socials/Pull-Requests.html"><i class="mdi mdi-github"></i> Pull Requests</a>\
+        <a href="/Socials/Issues.html"><i class="mdi mdi-github"></i> Issues</a>\
+        <a href="/Socials/Discord.html"><i class="mdi mdi-discord text--color-discord"></i> Discord</a>\
+        <a><i class="mdi mdi-youtube text--color-youtube"></i> YouTube</a>\
+      </nav>\
+  </div>\
 </footer>\
 \
 ');
