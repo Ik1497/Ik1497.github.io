49fe02e1ead6db30ecbc494dcb4feee39c872076
Ik <113038466+Ik1497@users.noreply.github.com>
2022-09-24 18:19:06 +0200

    Major NavBar changes

--- assets/navigation.js
+++ assets/navigation.js
@@ -1,11 +1,12 @@
 document.write('\
 \
 <ul class="navbar">\
-<a href="/About.html"><li class="mdi mdi-home">Home</li></a>\
-<details><summary>Extensions</summary>\
-  <a "/Extensions/Discord-Webhooks.html"><li>Discord Webhooks</li></a>\
-</details>\
+<a href="/index.html"><li class="mdi mdi-home">Home</li></a>\
 <a href="/About.html"><li class="mdi mdi-format-quote-open">About</li></a>\
+<hr>\
+<details><summary>Extensions</summary>\
+  <a "/Extensions/Discord-Webhooks.html"><li class="mdi mdi-forum">Discord Webhooks</li></a>\
+</details>\
 </ul>\
 \
 ');
