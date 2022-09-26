0a0f2937c2a1d166dd54efee6b6e3b26d16953c0
Ik <113038466+Ik1497@users.noreply.github.com>
2022-09-26 17:46:30 +0200

    404 page

--- assets/head.js
+++ assets/head.js
@@ -1,20 +1,18 @@
-document.write('\
-\
-<meta charset="UTF-8">\
-<meta http-equiv="X-UA-Compatible" content="IE=edge">\
-<meta name="viewport" content="width=device-width, initial-scale=1.0">\
-<meta name="og:site_name" content="Streamer.bot Actions"/>\
-<meta property="og:type" content="website" />\
-<meta property="og:image" content="https://raw.githubusercontent.com/Ik1497/Ik1497/main/assets/profile_picture/Logo-Round.png" />\
-<link rel="icon" href="https://raw.githubusercontent.com/Ik1497/Ik1497/main/assets/profile_picture/Logo-Round.png" />\
-<meta name="theme-color" content="#B80086">\
-<link rel="stylesheet" href="/assets/style.css">\
-<script src="/assets/navigation.js"></script>\
-<script src="/assets/header.js"></script>\
-<link rel="stylesheet" href="/assets/node_modules/@mdi/font/css/materialdesignicons.css">\
-\
-<script src="/assets/Code-Styles/prism.js"></script>\
-<script src="/assets/Code-Styles/code-copy.js"></script>\
-<link rel="stylesheet" href="/assets/Code-Styles/prism.css">\
-\
-');
+document.write(`
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="og:site_name" content="Streamer.bot Actions"/>
+<meta property="og:type" content="website" />
+<meta property="og:image" content="https://raw.githubusercontent.com/Ik1497/Ik1497/main/assets/profile_picture/Logo-Round.png" />
+<link rel="icon" href="https://raw.githubusercontent.com/Ik1497/Ik1497/main/assets/profile_picture/Logo-Round.png" />
+<meta name="theme-color" content="#B80086">
+<link rel="stylesheet" href="/assets/style.css">
+<script src="/assets/navigation.js"></script>
+<script src="/assets/header.js"></script>
+<link rel="stylesheet" href="/assets/node_modules/@mdi/font/css/materialdesignicons.css">
+
+<script src="/assets/Code-Styles/prism.js"></script>
+<script src="/assets/Code-Styles/code-copy.js"></script>
+<link rel="stylesheet" href="/assets/Code-Styles/prism.css">
+`);
--- assets/navigation.js
+++ assets/navigation.js
@@ -1,12 +1,10 @@
-document.write('\
-\
-<ul class="navbar">\
-<a href="/"><li class="mdi mdi-home">Home</li></a>\
-<a href="/About.html"><li class="mdi mdi-format-quote-open">About</li></a>\
-<hr>\
-<details><summary>Extensions</summary>\
-  <a "/Extensions/Discord-Webhooks.html"><li class="mdi mdi-forum">Discord Webhooks</li></a>\
-</details>\
-</ul>\
-\
-');
+document.write(`
+<ul class="navbar">
+<a href="/"><li class="mdi mdi-home">Home</li></a>
+<a href="/About.html"><li class="mdi mdi-format-quote-open">About</li></a>
+<hr>
+<details><summary>Extensions</summary>
+  <a href="/Extensions/Time-Widget.html"><li class="mdi mdi-clock-outline">Time Widget</li></a>
+</details>
+</ul>
+`);
