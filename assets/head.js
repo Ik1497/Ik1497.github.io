64efbb7472f72efa118f3996c9df71aa61c7f989
Ik <113038466+Ik1497@users.noreply.github.com>
2022-09-30 20:31:42 +0200

    Small header fixes

--- assets/head.js
+++ assets/head.js
@@ -18,7 +18,7 @@ document.write(`
 
 <header class="main-header">
 <div class="header-main-contents">
-<a class="title">
+<a class="title" href="/">
     <img src="/assets/favicon.ico" alt="favicon">
     <p>Streamer.bot Actions</p>
 </a>
