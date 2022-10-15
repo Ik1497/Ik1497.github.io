67bdc78be602967b72e97e48f17c146571b108a3
Ik <113038466+Ik1497@users.noreply.github.com>
2022-10-16 00:35:51 +0200

    Small Fixes

--- assets/head.js
+++ assets/head.js
@@ -8,22 +8,8 @@ document.write(`
 <link rel="icon" href="https://raw.githubusercontent.com/Ik1497/Ik1497/main/assets/profile_picture/Logo-Round.png" />
 <meta name="theme-color" content="#B80086">
 
-<header class="main-header">
-<div class="header-main-contents">
-<a class="title" href="/">
-    <img src="/assets/favicon.ico" alt="favicon">
-    <p>Streamer.bot Actions</p>
-</a>
-<ul class="nav-lists one">
-<li>Socials</li>
-<li><a href="/Socials/Discord.html">Discord</a></li>
-<li><a href="/Socials/Issues.html">Issues</a></li>
-<li><a href="/Socials/Pull-Requests.html">Pull Requests</a></li>
-<ul>
-</div>
-</header>
-
 <script src="/assets/navigation.js"></script>
+<script src="/assets/header.js"></script>
 <script src="/assets/theme.js"></script>
 <script src="/assets/main.js"></script>
 <link rel="stylesheet" href="/assets/node_modules/@mdi/font/css/materialdesignicons.css">
@@ -32,16 +18,3 @@ document.write(`
 <link rel="stylesheet" href="/assets/Prism/prism.css">
 <link rel="stylesheet" href="/assets/style.css">
 `);
-
-function GoWMan() {
-    var themeSettings = document.getElementById("themeSettings").value;
-    console.log(themeSettings);
-    if (themeSettings === "GoWMan") {
-    document.write(`
-    <link rel="stylesheet" href="/assets/style.css">
-    `);
-    }
-}
-{/* <div class="buttons">
-    <a id="dark-mode-toggle" class="dark-mode-toggle" onclick="themeSelector()"><i class="mdi mdi-invert-colors"></i></a>
-</div> */}
---
+++ assets/header.js
@@ -0,0 +1,10 @@
+document.write(`
+<header class="main-header">
+<div class="header-main-contents">
+<a class="title" href="/">
+    <img src="/assets/favicon.ico" alt="favicon">
+    <p>Streamer.bot Actions</p>
+</a>
+</div>
+</header>
+`);
