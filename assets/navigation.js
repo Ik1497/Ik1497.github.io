5912bb91e90ad52c9555c4077554ed413f4106e1
Ik <discord.ik1497@gmail.com>
2022-11-06 00:05:03 +0100

    Small Fixes

--- assets/navigation.js
+++ assets/navigation.js
@@ -1,6 +1,8 @@
 document.write(`
 <ul class="navbar">
 <a href="/"><li class="mdi mdi-home">Home</li></a>
+<a href="/Changelogs/"><li class="mdi mdi-fire">Changelogs (WIP)</li></a>
+<hr>
 <a href="/Extensions/Music"><li class="mdi mdi-spotify">Music Widget</li></a>
 </ul>
 `);
