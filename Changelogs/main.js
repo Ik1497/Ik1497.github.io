dd99fbd7a37fa0316124e75ac7f35a9ca8f7366e
Ik <discord.ik1497@gmail.com>
2022-11-06 19:41:49 +0100

    Small Fixes

--- Changelogs/main.js
+++ Changelogs/main.js
@@ -1,12 +1,23 @@
 WidgetsMusic();
 
-/*  ===============
-    Widgets 
-    ===============*/
+///////////////////
+/// > Widgets < ///
+///////////////////
 
 // Music
 async function WidgetsMusic() {
     const resp = await fetch("./Widgets-Music.html");
+    const main = await resp.text();
+    document.querySelector("main").insertAdjacentHTML("beforeend", main);
+}
+
+//////////////////////
+/// > Extensions < ///
+//////////////////////
+
+// MusicBee
+async function ExtensionsMusicBee() {
+    const resp = await fetch("./Extensions-MusicBee.html");
     const main = await resp.text();
     document.querySelector("main").insertAdjacentHTML("beforeend", main);
 }
