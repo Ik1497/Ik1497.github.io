7fb9153a2d2915334aea9c12547d93f8cb822dbb
Ik <discord.ik1497@gmail.com>
2022-11-08 17:04:59 +0100

    Small Fixes

--- Changelogs/main.js
+++ Changelogs/main.js
@@ -6,16 +6,16 @@ changelogSelector();
 
 // Music
 async function WidgetsMusic() {
-    const resp = await fetch("./pages/Widgets/Music.html");
-    const main = await resp.text();
-    document.querySelector("main").insertAdjacentHTML("beforeend", main);
+  const resp = await fetch("./pages/Widgets/Music.html");
+  const main = await resp.text();
+  document.querySelector("main").insertAdjacentHTML("beforeend", main);
 }
 
 // Test
 async function WidgetsTest() {
-    const resp = await fetch("./pages/Widgets/Test.html");
-    const main = await resp.text();
-    document.querySelector("main").insertAdjacentHTML("beforeend", main);
+  const resp = await fetch("./pages/Widgets/Test.html");
+  const main = await resp.text();
+  document.querySelector("main").insertAdjacentHTML("beforeend", main);
 }
 
 //////////////////////
@@ -24,23 +24,43 @@ async function WidgetsTest() {
 
 // MusicBee
 async function ExtensionsMusicBee() {
-    const resp = await fetch("./pages/Extensions/MusicBee.html");
-    const main = await resp.text();
-    document.querySelector("main").insertAdjacentHTML("beforeend", main);
+  const resp = await fetch("./pages/Extensions/MusicBee.html");
+  const main = await resp.text();
+  document.querySelector("main").insertAdjacentHTML("beforeend", main);
 }
 
 function changelogSelector() {
-    var element = document.getElementById("selectChangelogExtension");
-    var value = element.value;
-    var main = document.querySelector("main");
-    main.parentNode.removeChild(main);
+  var element = document.getElementById("selectChangelogExtension");
+  var value = element.value;
+  var main = document.querySelector("main");
+  main.parentNode.removeChild(main);
 
-    var newMain = '<main class=""></main>'
-    document.querySelector("body").insertAdjacentHTML("afterbegin", newMain);
+  var newMain = '<main class=""></main>';
+  document.querySelector("body").insertAdjacentHTML("afterbegin", newMain);
 
+  if (value === "WidgetsMusic") {
+    WidgetsMusic();
+  }
+  if (value === "WidgetsTest") {
+    WidgetsTest();
+  }
 
-    if (value === "WidgetsMusic") {WidgetsMusic();}
-    if (value === "WidgetsTest") {WidgetsTest();}
-
-    if (value === "ExtensionsMusicBee") {ExtensionsMusicBee();}
+  if (value === "ExtensionsMusicBee") {
+    ExtensionsMusicBee();
+  }
 }
+
+//////////////////
+/// Default JS ///
+//////////////////
+
+/*
+var changelogList = document.querySelectorAll(
+  ".changelog-new, .changelog-update, .changelog-fixes"
+);
+
+for (var i = 0; i < changelogList.length; ++i) {
+  console.log(i);
+  changelogList[i].classList.add("animated fadeInUp wait-p6s");
+}
+*/
