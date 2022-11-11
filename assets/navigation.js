7838e1b3928045bb4a66ff78b3bb0bc5386e868a
Ik <discord.ik1497@gmail.com>
2022-11-11 20:39:52 +0100

    Small Fixes

--- assets/navigation.js
+++ assets/navigation.js
@@ -4,6 +4,6 @@ document.write(`
 <a href="/Changelogs/"><li class="mdi mdi-fire">Changelogs (WIP)</li></a>
 <hr>
 <a href="/Extensions/Music"><li class="mdi mdi-spotify">Music Widget</li></a>
-<a target="_blank" href="https://extensions.streamer.bot/en/extensions/Mute-Indicator"><li class="mdi mdi-volume-mute">Mute Indicator</li></a>
+<a href="/Extensions/Mute-Indicator/docs"><li class="mdi mdi-volume-mute">Mute Indicator</li></a>
 </ul>
 `);
