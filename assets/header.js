70f82dfc92dfc2ace9e10d5759c1d0f8efcd5d2c
Ik <discord.ik1497@gmail.com>
2022-11-11 21:45:54 +0100

    Small Fixes

--- assets/header.js
+++ assets/header.js
@@ -3,7 +3,7 @@ document.write(`
 <div class="header-main-contents">
 <a class="title" href="/">
     <img src="/assets/favicon.ico" alt="favicon">
-    <p>Streamer.bot Actions</p>
+    <p>Streamer.bot Actions<span>By ik#1497 (on discord)</span></p>
 </a>
 </div>
 </header>
