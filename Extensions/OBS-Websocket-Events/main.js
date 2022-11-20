634774e5d5a73b42a13321082d175d89267e9739
Ik <discord.ik1497@gmail.com>
2022-11-20 02:07:56 +0100

    Small Fixes

--- Extensions/OBS-Websocket-Events/main.js
+++ Extensions/OBS-Websocket-Events/main.js
@@ -18,23 +18,24 @@ var getJSON = function(url, callback) {
 function navClick() {
     setTimeout(function () {
         location.reload();
-    }, 100)
-}
+    }, 100);
+};
 /// Fetch Protocol ///
 
 document.body.insertAdjacentHTML(`afterbegin`, `<nav class="obs-events-nav"></nav>`)
-// const params = window.location.hash;
 
 getJSON('https://raw.githubusercontent.com/obsproject/obs-websocket/master/docs/generated/protocol.json',
-function(err, data, hash) {
+function(err, data) {
     console.log(data);
     for (var eventsLength = 0; eventsLength < data.events.length; eventsLength++) {
         let events = data.events[eventsLength];
         // NavBar
-        document.querySelector(`.obs-events-nav`).insertAdjacentHTML(`beforeend`, `<li data-category="` + events.category + `" class="obs-event-list-item category-` + events.category + `"><a href="#` + events.eventType + `" onclick="navClick()">` + events.eventType + `</a></li>`);
-
         let hash = window.location.hash.replace("#","");
+        document.querySelector(`.obs-events-nav`).insertAdjacentHTML(`beforeend`, `<li class="obs-event-list-item category-` + events.category + ` item-` + events.eventType + `"><a href="#` + events.eventType + `" onclick="navClick()"><p class="obs-event-list-item-title">` + events.eventType + `</p><p class="obs-event-list-item-description">` + events.description + `</p></a></li>`);
+
         if(hash === events.eventType) {
+            document.querySelector(`.item-` + events.eventType).classList.add("obs-events-nav-active");
+
             document.querySelector(".obs-events-nav").insertAdjacentHTML(`afterend`, `<main class="obs-events-data"></main>`);
             document.querySelector(".obs-events-data").insertAdjacentHTML(`afterbegin`, `<section class="obs-events-data-top-section"><h1 class="obs-events-data-top-section-title">` + events.eventType + `</h1><p class="obs-events-data-top-section-group">` + events.category + `</p><p class="obs-events-data-top-section-description">` + events.description.replace("\n","<br>") + `</p></section>`);
             document.querySelector(".obs-events-data-top-section").insertAdjacentHTML(`afterend`, `<section class="obs-events-data-fields-table"><h2>Data Fields</h2><table><thead class="obs-events-data-fields-table-thead"><tr><td class="table-right">Name</td><td class="table-center">Description</td></tr></thead><tbody class="obs-events-data-fields-table-tbody"><tr><td class="table-right">Complexity Rating:</td><td class="table-center stars stars--` + events.complexity + `" style="--rating: ` + events.complexity + `;"></td></tr><tr><td class="table-right">Latest Supported RPC Version:</td><td class="table-center"><em class="obs-version-badge">` + events.rpcVersion + `</em></td></tr><tr><td class="table-right">Added in:</td><td class="table-center"><em class="obs-version-badge">` + events.initialVersion + `</em></td></tr></tbody></table></section>`);
@@ -44,7 +45,7 @@ function(err, data, hash) {
 
             for (var variableFieldsLength = 0; variableFieldsLength < events.dataFields.length; variableFieldsLength++) {
                 let variableFields = events.dataFields[variableFieldsLength];
-                document.querySelector(".obs-events-variables-table-tbody").insertAdjacentHTML(`beforeend`, `<tr><td class="table-right"><code class="code datatype">` + variableFields.valueName + `</code></td><td class="table-center"><code class="datatype">` + variableFields.valueType.replace("<Array>", `<em class="datatype-data">Array</em>`).replace("<Object>", `<em class="datatype-data">Object</em>`) + `</code></td><td class="table-left">` + variableFields.valueDescription + `</td></tr>`);
+                document.querySelector(".obs-events-variables-table-tbody").insertAdjacentHTML(`beforeend`, `<tr><td class="table-right"><code class="code datatype">obsEvent.` + variableFields.valueName + `</code></td><td class="table-center"><code class="datatype">` + variableFields.valueType.replace("<Array>", `<em class="datatype-data">Array</em>`).replace("<Object>", `<em class="datatype-data">Object</em>`) + `</code></td><td class="table-left">` + variableFields.valueDescription + `</td></tr>`);
             }
         }
     };
