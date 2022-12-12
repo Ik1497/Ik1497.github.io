754be1d822ac4c33f3066d667ab29d03ba7f58be
Ik <113038466+Ik1497@users.noreply.github.com>
2022-12-12 22:39:32 +0100

    Small Fixes

---
+++ index.js
@@ -0,0 +1,44 @@
+let items = [
+    {
+        name: "Mute Indicator",
+        description: "Mute Indicator so you can see if and what sources are muted.",
+        icon: "mdi mdi-volume-mute",
+        href: "/Extensions/Mute-Indicator/Docs",
+        enabled: true
+    },
+    {
+        name: "Music Widget",
+        description: "Music Widget for Spotify",
+        icon: "mdi mdi-spotify",
+        href: "/Extensions/Music/",
+        enabled: true
+    },
+    {
+        name: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet.",
+        icon: "mdi mdi-solid",
+        href: "/",
+        enabled: false
+    },
+    {
+        name: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet.",
+        icon: "mdi mdi-solid",
+        href: "/",
+        enabled: false
+    },
+    {
+        name: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet.",
+        icon: "mdi mdi-solid",
+        href: "/",
+        enabled: false
+    },
+    {
+        name: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet.",
+        icon: "mdi mdi-solid",
+        href: "/",
+        enabled: false
+    }
+]
