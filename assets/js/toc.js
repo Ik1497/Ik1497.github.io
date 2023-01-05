83703be39a8185c22d936ecd94591117243e33da
Ik <113038466+Ik1497@users.noreply.github.com>
2023-01-05 07:28:02 +0100

    Small Fixes

---
+++ assets/js/toc.js
@@ -0,0 +1,5 @@
+document.querySelector(`main`).insertAdjacentHTML(`afterend`, `<ul class="table-of-contents"></ul>`);
+
+document.querySelectorAll(`h2, h3, h4, h5, h6`).forEach(heading => {
+    document.querySelector(`ul.table-of-contents`).insertAdjacentHTML(`beforeend`, `<li class="heading-tag-${heading.tagName.toLowerCase()}"><button onclick="location.href = '#${heading.id}'"><span class="mdi mdi-chevron-right"></span>${heading.innerHTML}</button></li>`);
+});
