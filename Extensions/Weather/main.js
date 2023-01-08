ccaf37ba2c7bbb40af16de46f3b300d89cc52451
Ik <113038466+Ik1497@users.noreply.github.com>
2023-01-08 07:45:55 +0100

    Small Fixes

--- Extensions/Weather/main.js
+++ Extensions/Weather/main.js
@@ -1,40 +1,46 @@
 let iconMap = [
   {
-    name: "Sunny",
+    name: `Regular`,
+    id: `regular`,
     iconGroup: {day: [`day`], night: [`night`], misc: []},
-    weatherCodes: [0, 1]
+    weatherCode: [0, 1]
   },
   {
-    name: "Cloudy & Sunny",
+    name: `Cloudy & Regular`,
+    id: `cloud-regular`,
     iconGroup: {day: [`cloudy-day-1`, `cloudy-day-2`, `cloudy-day-3`], night: [`cloudy-night-1`, `cloudy-night-2`, `cloudy-night-3`], misc: []},
-    weatherCodes: [2]
+    weatherCode: [2]
   },
   {
-    name: "Cloudy",
+    name: `Cloudy`,
+    id: `cloud`,
     iconGroup: {day: [], night: [], misc: [`cloudy`]},
-    weatherCodes: [3]
+    weatherCode: [3]
   },
   {
-    name: "Foggy",
+    name: `Foggy`,
+    id: `fog`,
     iconGroup: {day: [], night: [], misc: [`cloudy`]},
-    weatherCodes: [45, 48]
+    weatherCode: [45, 48]
   },
   {
-    name: "Rainy",
+    name: `Rainy`,
+    id: `rain`,
     iconGroup: {day: [`rainy-1`, `rainy-2`, `rainy-3`], night: [`rainy-4`, `rainy-5`, `rainy-6`, `rainy-7`], misc: []},
-    weatherCodes: [51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82]
+    weatherCode: [51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82]
   },
   {
-    name: "Snowy",
+    name: `Snowy`,
+    id: `snow`,
     iconGroup: {day: [`snowy-1`, `snowy-2`, `snowy-3`], night: [`snowy-4`, `snowy-5`, `snowy-6`], misc: []},
-    weatherCodes: [71, 73, 75, 77, 85, 86]
+    weatherCode: [71, 73, 75, 77, 85, 86]
   },
   {
-    name: "Thunderstorm",
+    name: `Thunderstorm`,
+    id: `thunder`,
     iconGroup: {day: [], night: [], misc: [`thunder`]},
-    weatherCodes: [95, 96, 99]
-  },
-
+    weatherCode: [95, 96, 99]
+  }
 ]
 
 app()
@@ -43,26 +49,50 @@ async function app() {
   let weather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=52.382158355677774&longitude=4.887103035690015&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m,weathercode`)
   weather = await weather.json()
 
-  let icon = {icon: `rainy-1`, timerPeriod: `Misc`}
+  console.log(`Fetched weather:`, weather)
+
   let temperature = weather.current_weather.temperature
-  let weathercode = weather.current_weather.weathercode
+  let weatherCode = weather.current_weather.weathercode
 
   let day = weather.current_weather.time.split(`T`)[1]
 
   day = day.split(`:`)[0]
-  if (day >= 6 && day <= 18) {
+  if (day >= 7 && day <= 21) {
     day = `day`
   } else {
     day = `night`
   }
 
   let iconType = `animated`
-  if (new URLSearchParams(window.location.search).get("animated") != undefined) iconType = `animated`
-  
-  console.log(weathercode)
+  if (new URLSearchParams(window.location.search).get(`animated`) != undefined) iconType = `animated`
+
+  let randomIcon;
+  let weatherName;
+
   iconMap.forEach(iconMapIcon => {
-    
+    iconMapIcon.weatherCode.forEach(iconMapIconWeatherCode => {
+      if (iconMapIconWeatherCode === weatherCode) {
+
+        weatherName = iconMapIcon.name
+        if (iconMapIcon.iconGroup.misc.toString() === ``) {
+          
+          if (day === `day`) {
+            randomIcon = iconMapIcon.iconGroup.day[Math.floor(Math.random() * iconMapIcon.iconGroup.day.length)];
+          }
+          
+          if (day === `night`) {
+            randomIcon = iconMapIcon.iconGroup.night[Math.floor(Math.random() * iconMapIcon.iconGroup.night.length)];
+          }
+          
+        } else {
+          randomIcon = iconMapIcon.iconGroup.misc[Math.floor(Math.random() * iconMapIcon.iconGroup.misc.length)];
+        }
+
+      }
+    });
   });
+
+  document.body.setAttribute(`data-time`, day)
   
-  document.body.insertAdjacentHTML(`afterbegin`, `<div class="weather-widget"><img class="weather-icon" src="./icons/${iconType}/${icon.icon}.svg"><p class="weather-subtitle">${icon.timerPeriod}</p><p class="temperature">${temperature}</p></div>`)
+  document.body.insertAdjacentHTML(`afterbegin`, `<div class="weather-widget"><img class="weather-icon" src="./icons/${iconType}/${randomIcon}.svg"><p class="weather-subtitle">${weatherName}</p><p class="temperature">${temperature}</p></div>`)
 }
--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,5 +1,9 @@
-document.querySelector(`main`).insertAdjacentHTML(`afterend`, `<ul class="table-of-contents"></ul>`);
+let headings = document.querySelectorAll(`h2, h3, h4, h5, h6`)
 
-document.querySelectorAll(`h2, h3, h4, h5, h6`).forEach(heading => {
-    document.querySelector(`ul.table-of-contents`).insertAdjacentHTML(`beforeend`, `<li class="heading-tag-${heading.tagName.toLowerCase()}" title="${heading.innerText}" aria-label="${heading.innerText}"><button onclick="location.href = '#${heading.id}'"><span class="mdi mdi-chevron-right"></span>${heading.innerText}</button></li>`);
-});
+if (headings.length != 0) {
+    document.querySelector(`main`).insertAdjacentHTML(`afterend`, `<ul class="table-of-contents"></ul>`);
+    
+    headings.forEach(heading => {
+        document.querySelector(`ul.table-of-contents`).insertAdjacentHTML(`beforeend`, `<li class="heading-tag-${heading.tagName.toLowerCase()}" title="${heading.innerText}" aria-label="${heading.innerText}"><button onclick="location.href = '#${heading.id}'"><span class="mdi mdi-chevron-right"></span>${heading.innerText}</button></li>`);
+    });
+}
