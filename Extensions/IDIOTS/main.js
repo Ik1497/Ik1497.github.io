82d8322b03128abea4b1e3dc792e020830b74f42
Ik <113038466+Ik1497@users.noreply.github.com>
2023-01-09 16:17:35 +0100

    Small Fixes

--- Extensions/IDIOTS/main.js
+++ Extensions/IDIOTS/main.js
@@ -5,8 +5,14 @@ async function app() {
     idiots = await idiots.json()
     let randomIdiotsIndex = Math.floor(Math.random()*idiots.length)
     let idiot = idiots[randomIdiotsIndex];
+    if (new URLSearchParams(window.location.search).get(`force`) != null) {
+        idiots.forEach(idiotList => {
+            if (idiotList.user === new URLSearchParams(window.location.search).get(`force`)) {
+                idiot = idiotList
+            }
+        });
+    }
     document.querySelector(`main .idiot-image img.forground-image`).src = `./images/logos/${idiot.user}.png`
-    console.log(idiot, idiots)
 
     for (let imagesRunTime = 0; imagesRunTime < (idiot.images + 1); imagesRunTime++) {
         document.querySelector(`main .idiot-grid`).insertAdjacentHTML(`beforeend`, `<img src="./images/messages/${idiot.user}-${imagesRunTime}.png">`)
