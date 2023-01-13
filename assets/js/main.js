01d183382db9cc38ebaee3550aa44615b464bc7b
Ik <113038466+Ik1497@users.noreply.github.com>
2023-01-13 08:13:29 +0100

    Small Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,41 @@ document.write(`
 document.querySelectorAll(`a`).forEach(a => {
     a.classList.add( location.hostname === a.hostname || !a.hostname.length ? 'is-internal-link' : 'is-external-link' );
 });
+
+
+if (tableData != undefined) {
+    tableData.forEach(table => {
+        let uniqueTableHeads = []
+        let selectedTable = document.querySelector(`[data-table="${table.id}"]`)
+        selectedTable.innerHTML = `<thead><tr></tr></thead><tbody></tbody>`
+
+        table.contents.forEach(tableContent => {
+            Object.keys(tableContent).forEach(tableKey => {
+                if (!uniqueTableHeads.includes(tableKey)) {
+                    uniqueTableHeads.push(tableKey)
+                    selectedTable.querySelector(`thead tr`).innerHTML += `<th>${tableKey}</th>`
+                }
+            });
+
+            let td = `` 
+            Object.values(tableContent).forEach(tableBodyValues => {
+                td += `<td style="text-align: center;">${tableBodyValues}</td>`
+            });
+            console.log(td)
+            selectedTable.querySelector(`tbody`).innerHTML += `<tr>${td}</tr>`
+
+        });
+        selectedTable.querySelectorAll(`tbody tr td:first-child, thead tr th:first-child`).forEach(element => {
+            element.style.textAlign = `right`
+        });
+        selectedTable.querySelectorAll(`tbody tr td:last-child, thead tr th:last-child`).forEach(element => {
+            element.style.textAlign = `left`
+        });
+        selectedTable.querySelectorAll(`tbody tr td:not(:last-child)`).forEach(element => {
+            element.innerHTML = `<code>${element.innerHTML}</code>`
+        });
+    });
+}
+
+// <tr><td class="table-right">Name</td><td class="table-left">Description</td></tr>
+// <tr><td class="table-right"><code>font-family</code></td><td class="table-left">The font family <code>default: Uni Sans CAPS</code>.</td></tr>
