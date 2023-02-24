42fd7d50071e30e5ca638263c15e8e014ada5e06
Ik <113038466+Ik1497@users.noreply.github.com>
2023-02-25 00:06:05 +0100

    Small Fixes

--- components/tabset/main.js
+++ components/tabset/main.js
@@ -1,38 +1,58 @@
 function reloadTabSets() {
   document.querySelectorAll(`div.tabset:not([data-loaded])`).forEach(tabset => {
+    let tabsetTabCount = tabset.querySelectorAll(`div[data-tab]`).length
+    let tabsetGap = `0.25`
+
     tabset.setAttribute(`data-loaded`, ``)
     let tabset__Nav = document.createElement(`nav`)
     tabset.prepend(tabset__Nav)
 
     let tabset__Nav__Ul = document.createElement(`ul`)
+    tabset__Nav__Ul.style.gap = `${tabsetGap}rem`
     tabset__Nav.append(tabset__Nav__Ul)
+
+    let tabset__activeIndicator = document.createElement(`div`)
+    tabset__activeIndicator.className = `tabset-active-indicator`
+    tabset__activeIndicator.style.left = `0%`
+    tabset__activeIndicator.style.width = `calc(${100 / tabsetTabCount}% - ${tabsetGap}rem)`
+    tabset__Nav.append(tabset__activeIndicator)
+    
+    tabset.querySelectorAll(`div[data-tab]`).forEach((tab, tabIndex) => {
       
-      tabset.querySelectorAll(`div[data-tab]`).forEach(tab => {
-        let tabset__Nav__Ul__Li = document.createElement(`li`)
-        tabset__Nav__Ul.append(tabset__Nav__Ul__Li)
-        
-        let tabset__Nav__Ul__Li__Button = document.createElement(`button`)
-        tabset__Nav__Ul__Li__Button.innerHTML = tab.getAttribute(`data-tab`)
-        tabset__Nav__Ul__Li.append(tabset__Nav__Ul__Li__Button)
-        
-        tab.setAttribute(`hidden`, ``)
-        tabset.querySelector(`div[data-tab]`)?.removeAttribute(`hidden`)
-        tabset__Nav__Ul.querySelector(`li`).classList.add(`active`)
+      let tabset__Nav__Ul__Li = document.createElement(`li`)
+      tabset__Nav__Ul.append(tabset__Nav__Ul__Li)
+      
+      let tabset__Nav__Ul__Li__Button = document.createElement(`button`)
+      tabset__Nav__Ul__Li__Button.innerHTML = tab.getAttribute(`data-tab`)
+      tabset__Nav__Ul__Li.append(tabset__Nav__Ul__Li__Button)
+      
+      tab.setAttribute(`data-state`, `inactive`)
+      tabset.querySelector(`div[data-tab]`)?.setAttribute(`data-state`, `active`)
+      tabset__Nav__Ul.querySelector(`li`).classList.add(`active`)
 
-        tabset__Nav__Ul__Li__Button.addEventListener(`click`, () => {
-          tabset__Nav__Ul.querySelectorAll(`li.active`).forEach(listItem => {
-            listItem.classList.remove(`active`)
-          });
+      tabset__Nav__Ul__Li__Button.addEventListener(`click`, () => {
+        tabset__Nav__Ul.querySelectorAll(`li.active`).forEach(listItem => {
+          listItem.classList.remove(`active`)
+        });
 
-          tabset.querySelectorAll(`div[data-tab]`).forEach(tab => {
-            tab.setAttribute(`hidden`, ``)
-          });
+        tabset__activeIndicator.style.left = `${((100 / tabsetTabCount) * (tabIndex + 1)) - (100 / tabsetTabCount)}%`
 
-          tabset__Nav__Ul__Li.classList.add(`active`)
+        tabset.querySelectorAll(`div[data-tab][data-state="active"], div[data-tab][data-state="becoming-active"]`).forEach(tab => {
+          tab.setAttribute(`data-state`, `becoming-inactive`)
+          
+          setTimeout(() => {
+            tab.setAttribute(`data-state`, `inactive`)
+          }, 500);
+        });
 
-          tab.setAttribute(`hidden`, ``)
-          tab.removeAttribute(`hidden`)
-        })
-      });
+        tabset__Nav__Ul__Li.classList.add(`active`)
+
+        tab.setAttribute(`data-state`, `becoming-active`)
+          
+        setTimeout(() => {
+          tab.setAttribute(`data-state`, `active`)
+        }, 500);
+      })
+    });
   });  
 }
