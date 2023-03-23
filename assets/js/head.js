215a019238eb3567ddf3c1104539baa12c963f3d
Ik <113038466+Ik1497@users.noreply.github.com>
2023-03-24 00:20:26 +0100

    Small Fixes

--- assets/js/head.js
+++ assets/js/head.js
@@ -18,32 +18,7 @@ document.write(`
 <script src="/assets/js/toc.js" defer></script>
 <script src="/assets/js/image-handler.js" defer></script>
 
-<!-- Components -->
-
-<script src="/components/modal/main.js" defer></script>
-<link rel="stylesheet" href="/components/modal/style.css">
-
-<link rel="stylesheet" href="/components/overlay/style.css">
-<script src="/components/overlay/main.js" defer></script>
-
-<script src="/components/tabset/main.js" defer></script>
-<link rel="stylesheet" href="/components/tabset/style.css">
-
-<script src="/components/tabs/main.js" defer></script>
-<link rel="stylesheet" href="/components/tabs/style.css">
-
-<script src="/components/sb-import/main.js" defer></script>
-<link rel="stylesheet" href="/components/sb-import/style.css">
-
-<script src="/components/alerts/main.js" defer></script>
-<link rel="stylesheet" href="/components/alerts/style.css">
-
-<script src="/components/snackbar/main.js" defer></script>
-<link rel="stylesheet" href="/components/snackbar/style.css">
-
-<script src="/components/json-formatter/main.js"></script>
-
-<!-- End Components -->
+<script src="/components/main.js"></script>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.0.96/css/materialdesignicons.min.css">
 <link rel="stylesheet" href="/assets/css/style.css">
